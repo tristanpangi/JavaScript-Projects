fb574b059912cdefd1b2805ff2cff1bcd64aac86
tristanepang <82466679+tristanepang@users.noreply.github.com>
2025-04-28 19:30:27 -0700

    Implemented appendNumber function

--- JavaScript Calculator/script.js
+++ JavaScript Calculator/script.js
@@ -17,7 +17,8 @@ class Calculator {
     }
 
     appendNumber(number){
-
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation){
@@ -29,7 +30,7 @@ class Calculator {
     }
 
     updateDisplay(){
-
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 
 
@@ -42,3 +43,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
